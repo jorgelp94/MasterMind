d60c2f8d9c761638078562139939d7c82cb6206a
Jorge Perales <jorgeluis070294@hotmail.com>
2015-01-29 19:15:20 -0600

    Initial Commit

---
+++ MasterMind/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MasterMind
+//
+//  Created by Jorge Luis Perales on 29/01/15.
+//  Copyright (c) 2015 Jorge Luis Perales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
